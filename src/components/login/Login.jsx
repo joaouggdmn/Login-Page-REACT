da51e6fab02a1fdb88c87a1001933babdc40f9f9
João Pedro Uggioni <uggionidaminj@gmail.com>
2026-06-04 18:06:52 -0300

    Add useState and handleSubmit

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,31 +1,49 @@
 import { FaUser, FaLock } from "react-icons/fa";
+import { useState } from "react";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="container">
-      <form>
+      <form onSubmit={handleSubmit}>
         <h2>Acesse o sistema</h2>
         <div>
-          <input type="email" placeholder="E-mail" />
+          <input
+            type="email"
+            placeholder="E-mail"
+            onChange={(e) => setEmail(e.target.value)}
+          />
           <FaUser className="icon" />
         </div>
         <div>
-          <input type="password" placeholder="Senha" />
+          <input
+            type="password"
+            placeholder="Senha"
+            onChange={(e) => setPassword(e.target.value)}
+          />
           <FaLock className="icon" />
         </div>
 
         <div>
-            <label>
-                <input type="checkbox" />
-                Lembrar-me
-            </label>
-            <a href="#">Esqueceu sua senha?</a>
+          <label>
+            <input type="checkbox" />
+            Lembrar-me
+          </label>
+          <a href="#">Esqueceu sua senha?</a>
         </div>
 
         <button>Entrar</button>
 
         <div className="signup-link">
-            <p>Não tem uma conta? <a href="#">Cadastre-se</a></p>
+          <p>
+            Não tem uma conta? <a href="#">Cadastre-se</a>
+          </p>
         </div>
       </form>
     </div>
