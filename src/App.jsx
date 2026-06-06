f6aca965f3486b58127dc2dbdc69b07f39a31c00
João Pedro Uggioni <uggionidaminj@gmail.com>
2026-06-06 14:05:33 -0300

    Add Router Dom structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
-import Login from "./components/login/Login"
+import { Outlet, NavLink } from "react-router-dom";
 
-
-function App() {
+export default function App() {
+  const navLinkStyles = ({ isActive }) =>
+    `px-4 py-2 rounded-md transition-colors ${
+      isActive
+        ? "bg-cyan-300 text-slate-950 font-semibold"
+        : "text-gray-300 hover:bg-gray-700 hover:text-white"
+    }`;
 
   return (
     <div>
-      <Login />
+      <nav className="bg-gray-900 shadow-md p-4 flex justify-center gap-6">
+        <NavLink to="/" className={navLinkStyles}>
+          Home
+        </NavLink>
+        <NavLink to="/login" className={navLinkStyles}>
+          Login
+        </NavLink>
+      </nav>
+      <main>
+        <Outlet />
+      </main>
     </div>
-  )
+  );
 }
-
-export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import Login from "./pages/Login.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import ErrorPage from "./pages/ErrorPage.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <Home /> },
+      {
+        path: "login",
+        element: <Login />,
+      },
+    ],
+  },
+]);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />,
+);
