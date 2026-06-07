bb5b5d125d1fbdbb0762a3da7128fceec1fd6c32
João Pedro Uggioni <uggionidaminj@gmail.com>
2026-06-07 17:34:06 -0300

    Refactor Register component to use controlled inputs and improve error handling

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,23 +1,75 @@
-function Register(){
-    return(
-        <div>
-            <h1>Cadastro</h1>
-            <form>
-                <label>
-                    Nome completo:
-                    <input type="text" name="name" />
-                </label>
-                <label>
-                    Email:
-                    <input type="email" name="email" />
-                </label>
-                <label>
-                    Senha:
-                    <input type="password" name="password" />
-                </label>
-                <button type="submit">Cadastrar</button>
-            </form>
-        </div>
-    );
+import { useState } from "react";
+import Input from "../components/form/Input";
+import Button from "../components/form/Button";
+import { Link, useNavigate } from "react-router-dom";
+import useAuthContext from "../hooks/useAuthContext";
+
+function Register() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passConf, setPassConf] = useState("");
+  const [error, setError] = useState("");
+
+  const { signup } = useAuthContext();
+  const navigate = useNavigate();
+
+  const handleSignup = () => {
+    if (!name || !email || !password || !passConf) {
+      setError("Preencha todos os campos");
+      return;
+    } else if (password !== passConf) {
+      setError("As senhas não conferem");
+      return;
+    }
+
+    const res = signup(email, password);
+
+    if (res) {
+      setError(res);
+      return;
+    }
+
+    alert("Usuário cadastrado com sucesso!");
+    navigate("/login");
+  };
+
+  return (
+    <div>
+      <h1>Cadastro</h1>
+      <form>
+        <label>
+          Nome completo:
+          <Input
+            type="text"
+            placeholder="Seu nome completo"
+            value={name}
+            onChange={(e) => [setName(e.target.value), setError("")]}
+            name="name"
+          />
+        </label>
+        <label>
+          Email:
+          <Input
+            type="email"
+            placeholder="seu@email.com"
+            value={email}
+            onChange={(e) => [setEmail(e.target.value), setError("")]}
+            name="email"
+          />
+        </label>
+        <label>
+          Senha:
+          <Input type="password" name="password" />
+        </label>
+        <label>
+          Confirmar Senha:
+          <Input type="password" name="passConf" />
+        </label>
+        <label>{error}</label>
+        <Button Text="Cadastrar" onClick={handleSignup} />
+      </form>
+    </div>
+  );
 }
-export default Register
+export default Register;
